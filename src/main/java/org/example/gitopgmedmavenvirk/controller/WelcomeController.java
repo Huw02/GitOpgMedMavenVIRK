5193cac69b7c3e02b825f7905d226d2e9ddb3a15
huw02 <hannibal@ussing.com>
2025-02-24 11:30:14 +0100

    controller og html lavet

---
+++ src/main/java/org/example/gitopgmedmavenvirk/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package org.example.gitopgmedmavenvirk.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+
+
+
+    @GetMapping("")
+    public String getWelcome(){
+        return "welcome";
+    }
+
+}
