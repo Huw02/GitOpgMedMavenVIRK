924288320b8c9985d7b9a3384fc7f2e8e8fd260f
huw02 <hannibal@ussing.com>
2025-02-24 11:16:26 +0100

    Initial commit

---
+++ src/main/java/org/example/gitopgmedmavenvirk/GitOpgMedMavenVirkApplication.java
@@ -0,0 +1,13 @@
+package org.example.gitopgmedmavenvirk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitOpgMedMavenVirkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitOpgMedMavenVirkApplication.class, args);
+    }
+
+}
